13176177323e6afc069a3a31613abef8e51a329d
Food159 <earth2546555@gmail.com>
2025-01-30 00:42:55 +0700

    Update3
    Dialogue update2

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -7,20 +7,25 @@ using UnityEngine.SceneManagement;
 public class Dialog : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
+    public TextMeshProUGUI textNames;
+    public TextMeshProUGUI textContinue;
+    [TextArea(3,10)]
     public string[] lines;
+    public string[] names;
     public float textSpeed;
 
     private int index;
     private void Start()
     {
         textComponent.text = string.Empty;
+        textNames.text = string.Empty;
         StartDialog();
     }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if(textComponent.text == lines[index]) 
+            if(textComponent.text == lines[index] && textNames.text == names[index]) 
             {
                 NextLine();
             }
@@ -28,22 +33,54 @@ public class Dialog : MonoBehaviour
             {
                 StopAllCoroutines();
                 textComponent.text = lines[index];
+                textNames.text = names[index];
             }
+        }
+        if(textComponent.text == lines[index])
+        {
+            textContinue.gameObject.SetActive(true);
+        }
+        else
+        {
+            textContinue.gameObject.SetActive(false);
         }
     }
 
     void StartDialog()
     {
         index = 0;
+        UpdateName();
         StartCoroutine(TypeLine());
     }
     IEnumerator TypeLine()
     {
-        foreach (char c in lines[index].ToCharArray()) 
+        textComponent.text = string.Empty;
+        textNames.text = string.Empty;
+        int maxLength = Mathf.Max(names[index].Length, lines[index].Length);
+
+        for(int i = 0; i < maxLength; i++) 
         {
-            textComponent.text += c;
+            if(i < names[index].Length)
+            {
+                textNames.text += names[index][i];
+            }
+            if(i < lines[index].Length) 
+            {
+                textComponent.text += lines[index][i];
+            }
             yield return new WaitForSeconds(textSpeed);
         }
+        //foreach (char c in names[index].ToCharArray())
+        //{
+        //    textNames.text += c;
+        //    yield return new WaitForSeconds(textSpeed);
+        //}
+        //foreach (char c in lines[index].ToCharArray()) 
+        //{
+        //    textComponent.text += c;
+        //    yield return new WaitForSeconds(textSpeed);
+        //}
+        
     }
     void NextLine()
     {
@@ -51,12 +88,24 @@ public class Dialog : MonoBehaviour
         {
             index++;
             textComponent.text = string.Empty;
+            UpdateName();
             StartCoroutine(TypeLine());
         }
         else
         {
             //gameObject.SetActive(false);
             SceneManager.LoadScene("SceneGameOne");
+        }
+    }
+    void UpdateName()
+    {
+        if(index < names.Length) 
+        {
+            textNames.text = names[index];
+        }
+        else 
+        {
+            textNames.text = string.Empty;
         }
     }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject panel;
+    public bool _isMenu;
+
+    private PlayerController playercontroller;
+    private PlayerShooting playershooting;
+    private void Awake()
+    {
+        playercontroller = FindObjectOfType<PlayerController>();
+        playershooting = FindObjectOfType<PlayerShooting>();
+    }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape)) 
         {
-            togglePanel();
+            Debug.Log("esc");
+            if(_isMenu)
+            {
+                closePanel();
+            }
+            else 
+            {
+                togglePanel();
+            }
+        }
+        if(!_isMenu)
+        {
+            EnablePlayercontrol();
+        }
+        else
+        {
+            DisablePlayercontrol();
         }
     }
     public void togglePanel()
@@ -17,6 +42,7 @@ public class PauseMenu : MonoBehaviour
         {
             panel.SetActive(true);
             Time.timeScale = 0f;
+            _isMenu = true;
         }
     }
     public void closePanel()
@@ -24,6 +50,17 @@ public class PauseMenu : MonoBehaviour
         {
             panel.SetActive(false);
             Time.timeScale = 1f;
+            _isMenu = false;
         }
+    }
+    private void DisablePlayercontrol()
+    {
+        if (playercontroller != null) playercontroller.enabled = false;
+        if (playershooting != null) playershooting.enabled = false;
+    }
+    private void EnablePlayercontrol()
+    {
+        if (playercontroller != null) playercontroller.enabled = true;
+        if (playershooting != null) playershooting.enabled = true;
     }
 }
