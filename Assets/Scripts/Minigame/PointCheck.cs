31605b6229ced05f0d6bde478f5ff792b9299803
Food159 <earth2546555@gmail.com>
2025-07-13 02:49:13 +0700

    minigameUpdate

--- Assets/Scripts/Minigame/PointCheck.cs
+++ Assets/Scripts/Minigame/PointCheck.cs
@@ -7,32 +7,39 @@ public class PointCheck : MonoBehaviour
 {
     [Header("UI")]
     public RectTransform pointer;
-    public RectTransform greenZone;
+    public List<RectTransform> greenZonePrefabs;
+    public RectTransform greenZoneParent;
 
     [Header("Config")]
     public float moveSpeed = 100f;
     public float minAngle = -90f;
     public float maxAngle = 90f;
+    public int greenZoneCount = 5;
+    public float greenZoneWidth = 20f;
     public KeyCode keyInput = KeyCode.Space;
 
     private bool rotating = false;
     private float currentAngle;
+    private List<RectTransform> activeGreenZone = new List<RectTransform>();
     private void Start()
     {
         StartCheck(); //test
-        currentAngle = minAngle;
-        pointer.transform.eulerAngles = new Vector3(0, 0, minAngle);
     }
     private void Update()
     {
         if (!rotating)
             return;
-        currentAngle += moveSpeed * Time.deltaTime;
-        if(currentAngle > maxAngle)
+
+        float angleDiff = Mathf.DeltaAngle(minAngle, maxAngle);
+        float direction = Mathf.Sign(angleDiff);
+        currentAngle += direction * moveSpeed * Time.deltaTime;
+
+        if((direction > 0 && currentAngle > maxAngle) || (direction < 0 && currentAngle < maxAngle))
         {
             currentAngle = minAngle;
         }
         pointer.transform.eulerAngles = new Vector3(0, 0, currentAngle);
+
         if(Input.GetKeyDown(keyInput)) 
         {
             CheckResult();
@@ -44,31 +51,53 @@ public class PointCheck : MonoBehaviour
         currentAngle = minAngle;
         pointer.transform.eulerAngles = new Vector3(0, 0, minAngle);
         gameObject.SetActive(true);
+
+        foreach(var zone in activeGreenZone) 
+        {
+            Destroy(zone.gameObject);
+        }
+        activeGreenZone.Clear();
+        for (int i = 0; i < greenZoneCount; i++)
+        {
+            float randomAngle = Random.Range(minAngle, maxAngle);
+            RectTransform randomPrefab = greenZonePrefabs[Random.Range(0, greenZonePrefabs.Count)];
+            RectTransform newZone = Instantiate(randomPrefab, greenZoneParent);
+            newZone.localEulerAngles = new Vector3(greenZoneWidth, newZone.sizeDelta.y);
+            activeGreenZone.Add(newZone);
+        }
     }
     void CheckResult()
     {
         rotating = false;
 
         float angle = pointer.localEulerAngles.z;
-        if(angle > 180f) 
+        if (angle > 180f)
         {
             angle -= 360f;
         }
 
-        float greenAngle = pointer.localEulerAngles.z;
-        if (greenAngle > 180f)
+        bool success = false;
+        foreach (var zone in activeGreenZone)
         {
-            greenAngle -= 360f;
+            float zoneAngle = zone.localEulerAngles.z;
+
+            if (zoneAngle > 180f)
+            {
+                zoneAngle -= 360f;
+            }
+            float halfRange = greenZoneWidth / 2;
+            float minRange = zoneAngle - halfRange;
+            float maxRange = zoneAngle + halfRange;
+
+            if (angle >= minRange && angle <= maxRange)
+            {
+                success = true;
+                break;
+            }
         }
-
-        float greenRange = greenZone.rect.width;
-
-        float greenMin = greenAngle - greenRange / 2f;
-        float greenMax = greenAngle + greenRange / 2f;
-
-        if (angle >= greenMin && angle <= greenMax)
+        if (success)
         {
-            Debug.Log("Success!!!");
+            Debug.Log("Success");
         }
         else
         {
