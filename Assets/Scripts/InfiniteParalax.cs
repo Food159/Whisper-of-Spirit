6f4372e8eee719b655bd99539577a56f2a295646
Food159 <earth2546555@gmail.com>
2025-08-28 01:08:00 +0700

    .

--- Assets/Scripts/InfiniteParalax.cs
+++ Assets/Scripts/InfiniteParalax.cs
@@ -12,16 +12,20 @@ public class InfiniteParalax : MonoBehaviour
         startpos = transform.position.x;
         lenght = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-    private void FixedUpdate()
+    private void Update()
     {
         float temp = (cam.transform.position.x * (1 - paralax));
         float dist = (cam.transform.position.x * paralax);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-        if (temp > startpos + lenght) 
+        if (temp > startpos + lenght)
+        {
             startpos += lenght;
-        else if (temp < startpos - lenght) 
+        }
+        else if (temp < startpos - lenght)
+        {
             startpos -= lenght;
+        }
     }
 }
